5806a3e590b23f12c83901d9b966a479b29a5d43
Carlos Ramirez <charlydeveloping@gmail.com>
2020-08-10 18:32:26 -0400

    Clase 1

---
+++ Layouts/app/src/main/java/com/zentechnology/layouts/LinearLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.zentechnology.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
